9a3dba3a37be5745449840e50fe03817b38cad47
“mehran <mehran.vejdanii@gmail.com>
2023-04-17 01:24:40 +0430

    get selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const formOpenBtn = document.querySelector(".form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw-hide");
