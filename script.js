ea38b37af469a51368ddbbd63911a91350db583d
“mehran <mehran.vejdanii@gmail.com>
2023-04-17 01:44:21 +0430

    added active style for switch the  login form and sign in

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ const pwShowHide = document.querySelectorAll(".pw-hide");
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
