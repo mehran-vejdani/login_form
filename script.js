0cfcbf2d064b5a5d79f0d5717c51eb9d0bc25e1c
“mehran <mehran.vejdanii@gmail.com>
2023-04-17 01:36:43 +0430

    add formopenBtn and closeBtn and added show style to css

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const formCloseBtn = document.querySelector(".form_close");
 const signupBtn = document.querySelector("#signup");
 const loginBtn = document.querySelector("#login");
 const pwShowHide = document.querySelectorAll(".pw-hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
